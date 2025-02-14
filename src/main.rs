007770ac98bd8e735fcacd35e546523936aaa852
luoyingbin <zebra@gmail.com>
2025-02-14 10:46:54 +0800

    feature:add support toml

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,14 @@ use rcli::{process_csv, Opts, Subcommand};
 fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
     match opts.cmd {
-        Subcommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+        Subcommand::Csv(opts) => {
+            let output = if let Some(output) = opts.output {
+                output.clone()
+            } else {
+                format!("output.{}", opts.format)
+            };
+            process_csv(&opts.input, output, opts.format)?;
+        }
     }
     Ok(())
 }
--- src/process.rs
+++ src/process.rs
@@ -1,7 +1,9 @@
-use cfg_if::cfg_if;
 use csv::Reader;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::fs;
+
+use crate::opts::OutputFormat;
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "PascalCase")]
@@ -15,28 +17,47 @@ struct Player {
     kit: u8,
 }
 
-pub fn process_csv(input: &str, output: &str) -> anyhow::Result<()> {
+pub fn process_csv(input: &str, output: String, format: OutputFormat) -> anyhow::Result<()> {
     let mut reader = Reader::from_path(input)?;
-    cfg_if! {
-        if #[cfg(feature = "collect")] {
-            /*
-                let records = reader
-                .deserialize()
-                .map(|record| record.unwrap())
-                .collect::<Vec<Player>>();
-           */
-            let records: Vec<Player> = reader.deserialize().collect::<Result<Vec<_>, _>>()?;
+    /*
+        cfg_if! {
+            if #[cfg(feature = "collect")] {
+                /*
+                    let records = reader
+                    .deserialize()
+                    .map(|record| record.unwrap())
+                    .collect::<Vec<Player>>();
+               */
+                let records: Vec<Player> = reader.deserialize().collect::<Result<Vec<_>, _>>()?;
+                let json = serde_json::to_string_pretty(&records)?;
+            } else {
+                let mut records = Vec::with_capacity(128);
+                for result in reader.deserialize() {
+                    let record: Player = result?;
+                    records.push(record);
+                }
             let json = serde_json::to_string_pretty(&records)?;
-        } else {
-            let mut records = Vec::with_capacity(128);
-            for result in reader.deserialize() {
-                let record: Player = result?;
-                records.push(record);
             }
-        let json = serde_json::to_string_pretty(&records)?;
         }
+    */
+    let mut records = Vec::with_capacity(128);
+    let headers = reader.headers()?.clone();
+    for result in reader.records() {
+        let record = result?;
+        // headers.iter() -> 使用 headers 的迭代器
+        // record.iter() -> 使用 record 的迭代器
+        // zip() -> 将两个迭代器合并为一个元组的迭代器 [(header, record), ..]
+        // collect::<Value>() -> 将元组的迭代器转换为 JSON Value
+        let json_value = headers.iter().zip(record.iter()).collect::<Value>();
+        records.push(json_value);
     }
 
-    fs::write(output, json)?;
+    let content = match format {
+        OutputFormat::Json => serde_json::to_string_pretty(&records)?,
+        OutputFormat::Yaml => serde_yaml::to_string(&records)?,
+        OutputFormat::Toml => toml::to_string(&records)?,
+    };
+
+    fs::write(output, content)?;
     Ok(())
 }
